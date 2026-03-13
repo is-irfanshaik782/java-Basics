9d62b1bb424da01becbcda718e5f20bb1699bd18
is-irfanshaik782 <is5442@srmist.edu.in>
2026-03-13 20:56:29 +0530

    Add files via upload

---
+++ AgeCalculator.java
@@ -0,0 +1,173 @@
+1. Harry's Age Calculator
+    // Creating AgeCalculator class to find the age based on birth and current year
+class AgeCalculator {
+    public static void main(String[] args) {
+        
+        // Create a variable to store the name of the person
+        String name = "Harry";
+        
+        // Create an int variable for birth year
+        int birthYear = 2000;
+        
+        // Create an int variable for the current year
+        int currentYear = 2024;
+        
+        // Calculate the age by subtracting birth year from current year
+        int age = currentYear - birthYear;
+        
+        // Display the result
+        System.out.println(name + "'s age in " + currentYear + " is " + age);
+    }
+} 
+
+
+
+
+
+
+2. PCM Average Marks
+    // Creating AverageMarks class to compute the average of PCM scores
+class AverageMarks {
+    public static void main(String[] args) {
+        
+        // Create variables for individual subject marks
+        int mathsMarks = 94;
+        int physicsMarks = 95;
+        int chemistryMarks = 96;
+        // Create a variable to store the number of subjects
+        int numberOfSubjects = 3;
+        
+        // Calculate total marks
+        int totalMarks = mathsMarks + physicsMarks + chemistryMarks;
+        
+        // Calculate average mark
+        double averageMark = (double) totalMarks / numberOfSubjects;
+        
+        // Display the result
+        System.out.println("Sam's average mark in PCM is " + averageMark);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                       
